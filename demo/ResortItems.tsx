f6149d734aa7f0901b67c70c6e7e30a2c15ff671
pinyin <bob432091@gmail.com>
2018-06-11 18:13:46 +0800

    add ResortItems demo

---
+++ demo/ResortItems.tsx
@@ -0,0 +1,94 @@
+import {existing} from '@pinyin/maybe'
+import {nothing} from '@pinyin/types'
+import randomcolor from 'randomcolor'
+import {CSSProperties, default as React} from 'react'
+import {getTweenState} from '../src/getTweenState'
+import {tweenHere} from '../src/tweenHere'
+import {TweenState} from '../src/TweenState'
+import {DemoProps} from './DemoProps'
+import {PlayDemo} from './PlayDemo'
+
+export class ResortItems extends React.Component<DemoProps, State, Snapshot> {
+    constructor(props: DemoProps) {
+        super(props)
+        this.state = {
+            items: new Array(10).fill(nothing).map((_, i) => ({id: i, color: randomcolor()}))
+        }
+    }
+
+    getSnapshotBeforeUpdate(): Snapshot {
+        const snapshots = new Map<number, TweenState>()
+        this.items.forEach((ref, id) =>
+            snapshots.set(id, getTweenState(ref))
+        )
+        return {items: snapshots}
+    }
+
+    render() {
+        const rootStyle = {
+            position: 'relative',
+            width: `${this.props.width}px`,
+            height: `${this.props.height}px`,
+            overflow: `hidden`
+        } as CSSProperties
+
+        const containerStyle = {
+            width: `${this.props.width}px`,
+            height: `${this.props.height}px`,
+            '-webkit-overflow-scrolling': 'touch',
+            overflowX: 'hidden',
+            overflowY: 'scroll',
+            willChange: 'transform'
+        } as CSSProperties
+
+        const contentStyle = {
+            width: `${this.props.width}px`
+        }
+
+        const itemStyle = (color: string) => ({
+            width: `${this.props.width}px`,
+            height: `${this.props.height / 4}px`,
+            backgroundColor: `${color}`
+        } as CSSProperties)
+
+        return <div style={rootStyle}>
+            <div style={containerStyle}>
+                <div style={contentStyle}>{
+                    this.state.items.map(({id, color}) =>
+                        <div key={id}
+                             ref={ref => existing(ref) ? this.items.set(id, ref) : this.items.delete(id)}
+                             style={itemStyle(color)}/>
+                    )
+                }</div>
+            </div>
+            <PlayDemo onClick={this.onClick}>
+                <svg style={{width: 24, height: 24}} viewBox="0 0 24 24">
+                    <path fill="#FFFFFF"
+                          d="M17,3L22.25,7.5L17,12L22.25,16.5L17,21V18H14.26L11.44,15.18L13.56,13.06L15.5,15H17V12L17,9H15.5L6.5,18H2V15H5.26L14.26,6H17V3M2,6H6.5L9.32,8.82L7.2,10.94L5.26,9H2V6Z"/>
+                </svg>
+            </PlayDemo>
+        </div>
+    }
+
+    componentDidUpdate(prevProps: DemoProps, prevState: State, snapshot: Snapshot) {
+        this.items.forEach((ref, id) =>
+            tweenHere(ref, snapshot.items.get(id), (from, to) => Math.abs(from.y - to.y))
+        )
+    }
+
+    private items = new Map<number, HTMLDivElement>()
+
+    private onClick = () => {
+        const prevItems = this.state.items
+        const shuffledItems = prevItems.sort((a, b) => Math.random() > 0.5 ? -1 : 1)
+        this.setState({items: shuffledItems})
+    }
+}
+
+type State = {
+    items: Array<{ id: number, color: string }>
+}
+
+type Snapshot = {
+    items: Map<number, TweenState>
+}
--- demo/index.tsx
+++ demo/index.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react'
 import {render} from 'react-dom'
 import {AnimatedScroll} from './AnimatedScroll'
+import {ResortItems} from './ResortItems'
 
 const root = document.getElementById("root") as HTMLDivElement
 
-render(<AnimatedScroll width={375} height={667}/>, root)
+render(<div>
+    <div style={{display: 'inline-block'}}><AnimatedScroll width={375} height={667}/></div>
+    <div style={{display: 'inline-block'}}><ResortItems width={375} height={667}/></div>
+</div>, root)
 
