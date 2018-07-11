2648e3fece7a9f2d08b35de6adbac9f2d0a18e8c
pinyin <bob432091@gmail.com>
2018-07-11 19:43:17 +0800

    add TweenableElement

---
+++ src/TweenableElement.ts
@@ -0,0 +1,1 @@
+export type TweenableElement = HTMLElement | SVGElement
--- src/getTweenState.ts
+++ src/getTweenState.ts
@@ -1,8 +1,9 @@
 import {hasStyle} from '@pinyin/dom'
 import {getOutline} from '@pinyin/outline'
+import {TweenableElement} from './TweenableElement'
 import {TweenState} from './TweenState'
 
-export function getTweenState(element: HTMLElement): TweenState {
+export function getTweenState(element: TweenableElement): TweenState {
     const outline = getOutline(element)
     const opacityCSS = getComputedStyle(element).opacity
     const opacity = hasStyle(opacityCSS) ? parseFloat(opacityCSS) : 1
