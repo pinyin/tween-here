30db0d1ef27481c71ee703e9ff4142ce179e60ee
pinyin <bob432091@gmail.com>
2018-07-11 19:42:51 +0800

    delete tweenEnd() for now

--- src/tweenEnd.ts
+++
@@ -1,9 +0,0 @@
-import {tweeningExit} from './tweenExit'
-import {tweeningHere} from './tweenHere'
-
-export function tweenEnd(element: HTMLElement): void {
-    tweeningHere.delete(element)
-    tweeningExit.delete(element)
-    element.style.transition = 'none'
-    element.style.transform = 'none'
-}
