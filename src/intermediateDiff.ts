805ca7af9303f7851f73aef178fd8435760b9929
pinyin <bob432091@gmail.com>
2018-07-17 08:08:34 +0800

    cleanup code

--- src/intermediateDiff.ts
+++
@@ -1,7 +0,0 @@
-import {composeDiff} from './composeDiff'
-import {inverseDiff} from './inverseDiff'
-import {TweenStateDiff} from './TweenStateDiff'
-
-export function intermediateDiff(from: TweenStateDiff, to: TweenStateDiff): TweenStateDiff {
-    return composeDiff(inverseDiff(from), to)
-}
--- src/inverseDiff.ts
+++
@@ -1,9 +0,0 @@
-import {inverse} from '@pinyin/outline'
-import {TweenStateDiff} from './TweenStateDiff'
-
-export function inverseDiff(diff: TweenStateDiff): TweenStateDiff {
-    return {
-        transform: inverse(diff.transform),
-        opacityDelta: -diff.opacityDelta,
-    }
-}
