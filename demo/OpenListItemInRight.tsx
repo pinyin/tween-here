7972190ad489965199ec3ac4a6dd4a1d8fa073c0
pinyin <bob432091@gmail.com>
2018-07-20 08:51:52 +0800

    us icons in demo

--- demo/OpenListItemInRight.tsx
+++ demo/OpenListItemInRight.tsx
@@ -58,7 +58,7 @@ export class OpenListItemInRight extends React.Component<DemoProps, State, Snaps
             width: this.props.width,
             height: this.props.height,
             backgroundColor: 'aliceblue',
-            textAlign: 'center',
+            textAlign: 'start',
             zIndex: 10,
         }
 
@@ -74,8 +74,7 @@ export class OpenListItemInRight extends React.Component<DemoProps, State, Snaps
                 // must provide key or this div will be reused unexpectedly
                 <div key={'page'} ref={this.item} style={openedItemStyle} onClick={this.onClick}>
                     <svg ref={this.text} style={{width: 96, height: 96}} viewBox="0 0 24 24">
-                        <path fill="#000000"
-                              d="M10,9A1,1 0 0,1 11,8A1,1 0 0,1 12,9V13.47L13.21,13.6L18.15,15.79C18.68,16.03 19,16.56 19,17.14V21.5C18.97,22.32 18.32,22.97 17.5,23H11C10.62,23 10.26,22.85 10,22.57L5.1,18.37L5.84,17.6C6.03,17.39 6.3,17.28 6.58,17.28H6.8L10,19V9M11,5A4,4 0 0,1 15,9C15,10.5 14.2,11.77 13,12.46V11.24C13.61,10.69 14,9.89 14,9A3,3 0 0,0 11,6A3,3 0 0,0 8,9C8,9.89 8.39,10.69 9,11.24V12.46C7.8,11.77 7,10.5 7,9A4,4 0 0,1 11,5Z"/>
+                        <path fill="#000000" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"/>
                     </svg>
                 </div> :
                 <div key={'list'} ref={this.list} style={listStyle}>
