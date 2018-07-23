5de7cf8e9ee1db18ebf3743c14f24f8092e7396c
pinyin <bob432091@gmail.com>
2018-07-23 14:32:37 +0800

    delete will-change

--- demo/OpenListItem.tsx
+++ demo/OpenListItem.tsx
@@ -51,7 +51,6 @@ export class OpenListItem extends React.Component<DemoProps, State, Snapshot> {
             width: `${this.props.width}px`,
             height: `${this.props.height / 6}px`,
             backgroundColor: `${color}`,
-            willChange: 'transform, opacity',
             textAlign: 'center',
         })
 
@@ -60,7 +59,6 @@ export class OpenListItem extends React.Component<DemoProps, State, Snapshot> {
             height: this.props.height,
             backgroundColor: 'aliceblue',
             textAlign: 'center',
-            willChange: 'transform, opacity',
             zIndex: 10,
         }
 
@@ -82,11 +80,7 @@ export class OpenListItem extends React.Component<DemoProps, State, Snapshot> {
                                      style={itemStyle('aliceblue')}
                                      onClick={this.onClick}
                                 >
-                                    <svg ref={this.text} style={{
-                                        width: 48,
-                                        height: 48,
-                                        willChange: 'transform, opacity',
-                                    }} viewBox="0 0 24 24">
+                                    <svg ref={this.text} style={{width: 48, height: 48}} viewBox="0 0 24 24">
                                         <path fill="#000000"
                                               d="M10,9A1,1 0 0,1 11,8A1,1 0 0,1 12,9V13.47L13.21,13.6L18.15,15.79C18.68,16.03 19,16.56 19,17.14V21.5C18.97,22.32 18.32,22.97 17.5,23H11C10.62,23 10.26,22.85 10,22.57L5.1,18.37L5.84,17.6C6.03,17.39 6.3,17.28 6.58,17.28H6.8L10,19V9M11,5A4,4 0 0,1 15,9C15,10.5 14.2,11.77 13,12.46V11.24C13.61,10.69 14,9.89 14,9A3,3 0 0,0 11,6A3,3 0 0,0 8,9C8,9.89 8.39,10.69 9,11.24V12.46C7.8,11.77 7,10.5 7,9A4,4 0 0,1 11,5Z"/>
                                     </svg>
